0cd5d82634d432d09b5afba861d47e7f5b36f5ae
alexandralondraliu <alexandra.londraliu@stud.acs.upb.ro>
2025-02-28 14:55:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -4,15 +4,14 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 import os
 
-# Image size and batch size
-IMG_SIZE = (48, 48)
-BATCH_SIZE = 32
+IMG_SIZE = (48, 48) 
+BATCH_SIZE = 32 # procss 32 images at a time
 
-# Data augmentation for better training
-train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, horizontal_flip=True)
-val_datagen = ImageDataGenerator(rescale=1./255)
+# set up data augmentation rules
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, horizontal_flip=True) # aply transformations to train data
+val_datagen = ImageDataGenerator(rescale=1./255) # only rescale validation data
 
-# Load dataset
+# load dataset and apply augmentation
 train_generator = train_datagen.flow_from_directory(
     "dataset/train",
     target_size=IMG_SIZE,
@@ -29,14 +28,17 @@ val_generator = val_datagen.flow_from_directory(
     class_mode="categorical"
 )
 
-# CNN Model
+# define cnn model
 model = Sequential([
+    #1st conv layer
     Conv2D(32, (3,3), activation='relu', input_shape=(48, 48, 1)),
     MaxPooling2D(2, 2),
     
+    #2nd conv layer
     Conv2D(64, (3,3), activation='relu'),
     MaxPooling2D(2, 2),
     
+    #3rd conv layer
     Conv2D(128, (3,3), activation='relu'),
     MaxPooling2D(2, 2),
     
@@ -46,13 +48,13 @@ model = Sequential([
     Dense(5, activation='softmax')  # 5 emotions → softmax for classification
 ])
 
-# Compile Model
+# compile model
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-# Train Model
+# train model
 model.fit(train_generator, validation_data=val_generator, epochs=10)
 
-# Save Model
+# save Model
 if not os.path.exists("model"):
     os.makedirs("model")
 
